e8f2648757512015e2e75ee369666878fdc89f03
Beatiz Lorente <beatriz.lorente@sitemercado.com.br>
2021-04-19 08:53:47 -0300

    Entrada do arquivo matriz, com a devida atribuição das variáveis e primeira interface gráfica.

---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame
+import tkinter as tk
+
+
+class field(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class robot(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        # horizontal lines
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
+
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+
+def message_box(subject, content):
+    pass
+
+
+def main():
+    global rows, width
+    width = 800
+    rows = 41
+    win = pygame.display.set_mode((width, width))
+    r = robot((255, 0, 0), (10, 10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
+        pass
+
+main()
---
+++ teste/teste_matriz.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+f = open('ArvEstrada.txt', 'r')
+
+start = f.readlines()[0]
+
+print(start)
+
+f.close()
+
+f = open('ArvEstrada.txt', 'r')
+
+goal = f.readlines()[1]
+
+print(goal)
+
+f.close()
+
+
+a_file = open("ArvEstrada.txt", "r")
+
+lines = a_file.readlines()
+a_file.close()
+
+del lines[1]
+
+new_file = open("field.txt", "w+")
+
+for line in lines:
+    new_file.write(line)
+
+new_file.close()
+
+a_file = open("field.txt", "r")
+
+lines = a_file.readlines()
+a_file.close()
+
+del lines[0]
+
+new_file = open("field.txt", "w+")
+
+for line in lines:
+    new_file.write(line)
+
+new_file.close()
+
+f = open("field.txt", "r")
+
+
+# start = list(map(int, f.))
+# entries = list(map(int, int.split(,)))
+
+#f = open('ArvEstrada.txt', 'r+')
+
+#d = f.readlines()
+
+#for i in d:
+#    if i != 0 or i != 1:
+#        f.write(i)
+#f.truncate()
+#f.close()
